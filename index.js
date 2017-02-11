e6af71b82aca60c02ec1145e463e0d618ccf19d8
Greg Allen <greg@firstandthird.com>
2017-02-11 15:57:14 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+const http = require('http');
+const port = process.env.PORT || 8081;
+
+const server = http.createServer((request, response) => {
+  process.env.VERSION=1;
+  response.end(JSON.stringify(process.env, null, '  '));
+});
+
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+process.on('SIGTERM', () => {
+  server.stop({ timeout: 5 * 1000 }, () => {
+    process.exit(0);
+  });
+});
