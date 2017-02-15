091caaeb965044cc4d5c3e411d70b30484c02399
Greg Allen <greg@firstandthird.com>
2017-02-15 10:54:26 -0800

    fixed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8081;
 
 const server = http.createServer((request, response) => {
   process.env.VERSION=1;
-  console.log(request.path.url);
+  console.log(request.url.path);
   response.end(JSON.stringify(process.env, null, '  '));
 });
 
