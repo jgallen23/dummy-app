455e99f24f3569e95a38900835bc4076fe6477ad
Greg Allen <greg@firstandthird.com>
2017-04-14 09:02:54 -0700

    Version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 8081;
 
 const server = http.createServer((request, response) => {
-  process.env.VERSION=9;
+  process.env.VERSION=10;
   console.log('incoming');
   response.end(JSON.stringify(process.env, null, '  '));
 });
